eece8ed2ba9b7482d3e6388d88d2d5abf3b17451
ankit <ankit_panchamia@hms.harvard.edu>
2016-04-08 13:47:45 -0400

    formatting dates for display.

--- web/src/main/webapp/js/wacct.js
+++ web/src/main/webapp/js/wacct.js
@@ -19,11 +19,24 @@ function displayCoverageList() {
         var out = "<div> <h2>Coverage List</h2> <ul> ";
         $.each(data, function (key, val) {
             var url = "/"+val.context+"/"+val.name;
-            out += " <li><a href='"+url+"'> " + new Date(parseInt(val.name)) + "</a></li> ";
+            out += " <li><a href='"+url+"'> " + getFormattedDate(new Date(parseInt(val.name))) + "</a></li> ";
         });
 
         out += " </ul> </div>";
         $("#coverageLists").html(out);
         $("#coverageLists").css({display: 'block'});
     });
+}
+
+function getFormattedDate(date) {
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    var hour = date.getHours().toString();
+    var min = date.getMinutes().toString();
+    var sec = date.getSeconds().toString();
+    var time = hour+':'+min+':'+sec;
+    return month + '/' + day + '/' + year + ' ' + time;
 }
