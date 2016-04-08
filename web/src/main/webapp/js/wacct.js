849de64983bde5a05c191fd004e8e52a17081a16
ankit <ankit_panchamia@hms.harvard.edu>
2016-04-08 12:45:55 -0400

    display list of coverage reports.

--- web/src/main/webapp/js/wacct.js
+++ web/src/main/webapp/js/wacct.js
@@ -1,33 +1,36 @@
 function captureData(event) {
     $.blockUI();
-    $.ajax({
-        url: 'rest/capture',
-        type: 'POST',
-        dataType: 'json',
-        data: '',
-        success: function (data) {
-            $.unblockUI();
-            alert("Capture Successful");
-        },
-        error: function (xhr, status, error) {
-            $.unblockUI();
-        }
-    })
+    $.post('rest/capture', {data: ''}, function (data) {
+        $.unblockUI();
+        alert("Capture Successful");
+        displayCoverageList();
+    });
 }
 
 function resetData(event) {
     $.blockUI();
-    $.ajax({
-        url: 'rest/reset',
-        type: 'POST',
-        dataType: 'json',
-        data: '',
-        success: function (data) {
-            $.unblockUI();
-            alert("Reset Successful");
-        },
-        error: function (xhr, status, error) {
-            $.unblockUI();
-        }
-    })
+    $.post('rest/reset', {data: ''}, function (data) {
+        $.unblockUI();
+        alert("Reset Successful");
+    });
+}
+
+function displayCoverageList() {
+    var out = "<div> <h2>Coverage List</h2> ";
+    $.post('rest/list', {data: ''}, function (data) {
+        console.log(data);
+        var parsedData = $.parseJSON(data);
+        console.log(parsedData);
+
+        out +=  " <ul> " +
+
+        $.each(parsedData, function (key, val) {
+            var url = "#"+val.context;
+            out += " <li><a href='"+url+"'> " + val.name + "</a></li> ";
+        });
+
+        out += " </ul> </div>";
+        $("#coverageList").html(out);
+        $("#coverageList").css({display: 'block'});
+    });
 }
